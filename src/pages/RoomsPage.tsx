7f79afd862b97ee50bf034802b47dd14f1b76e41
zaszuhaer2 <zas.zuhaer2@gmail.com>
2025-05-30 03:21:54 +0600

    Update RoomCard layout to be more compact

--- src/pages/RoomsPage.tsx
+++ src/pages/RoomsPage.tsx
@@ -58,9 +58,6 @@ const RoomsPage: React.FC = () => {
   
   return (
     <div className="space-y-6">
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-gray-900">Rooms</h1>
-      </div>
       
       <RoomFilters onFilterChange={setFilter} />
       
