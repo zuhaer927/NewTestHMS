1946804dd623a7bd1826538ef2f22b46dc0987e8
zuhaer927 <zuhaer927@gmail.com>
2025-05-30 22:42:35 +0600

    Deploy application to Netlify

--- src/components/rooms/RoomCard.tsx
+++ src/components/rooms/RoomCard.tsx
@@ -30,7 +30,8 @@ const RoomCard: React.FC<RoomCardProps> = ({ room, currentBooking, onClick, isAv
     >
       <div className="flex items-center justify-between">
         <div className="flex items-center space-x-4">
-          <h3 className="text-base font-semibold whitespace-nowrap">Room {room.roomNumber}</h3>
+          <h3 className="text-sm sm:text-base font-semibold whitespace-nowrap">
+Room {room.roomNumber}</h3>
           <div className="flex items-center space-x-3 text-gray-600">
             <div className="flex items-center">
               <BedDouble className="h-3 w-3 sm:h-4 sm:w-4" />
